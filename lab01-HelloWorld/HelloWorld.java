be622f68ef3b2d0405224d30268d4b1dc41dd96f
tiy319 <tiy319@lehigh.edu>
2015-08-28 15:51:25 +0000

    update HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,29 @@
+///////
+//Tiancheng Yang
+//Hello World java Program
+//
+//    First, compile the program 
+//          HelloWorld.java
+//    Then run the program 
+//          java HelloWorld //
+
+//      Define a class
+public class HelloWorld{
+    
+//      Add main method
+public static void main(String[ ] args)   {
+    
+//      Print the statement with a box around it
+        System.out.println("###########################");
+        System.out.println("###########################");
+        System.out.println("##                       ##");
+        System.out.println("##      Hello World      ##");
+        System.out.println("##                       ##");
+        System.out.println("###########################");
+        System.out.println("###########################");
+        System.out.println("test");
+    
+    //Test
+    
+}
+}
