9e7bf2f368f38940e56c0eca55bcf0d663240109
tiy319 <tiy319@lehigh.edu>
2015-08-31 17:05:05 +0000

    update hw01

---
+++ hw01/hw01.java
@@ -0,0 +1,37 @@
+////////
+//Tiancheng Yang
+//Homework No.1    Due Sep.1
+//WelcomeClass java program
+//
+//
+//  First, compile the program
+//      hw01.java
+//  Then, run the progam
+//      java hw01
+
+
+
+//Define a class
+    public class hw01{
+        
+//Add main method
+    public static void main(String[] args) {
+        
+//print our the content
+       
+        System.out.println(" -----------------");
+        System.out.println("      WELCOME     ");
+        System.out.println(" -----------------");
+        System.out.println("  ^  ^  ^  ^  ^  ^ ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-t--i--y--3--1--9->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("I love photography and I am planning to");
+        System.out.println("do a photography project on Lehigh campus.");
+        
+        //test
+        
+    }
+    
+    }
